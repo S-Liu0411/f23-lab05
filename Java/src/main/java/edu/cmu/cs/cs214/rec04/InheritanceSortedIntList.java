17407a5e36a0e5f7431c6ae90c297c8a5c1f4735
S-Liu0411 <zhexianl@andrew.cmu.edu>
2023-09-27 11:26:30 -0400

    completed inheritance for java

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -10,7 +10,44 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
 
+    private int totalAdded; // To keep track of the total attempted insertions
+    /**
+     * Constructs an empty InheritanceSortedIntList.
+     */
+    public InheritanceSortedIntList() {
+        super();
+        this.totalAdded = 0;
+    }
+
+    /**
+     * Adds an element to the list, incrementing the totalAdded count.
+     *
+     * @param value the value to be added
+     */
+    @Override
+    public boolean add(int value) {
+        this.totalAdded++;
+        return super.add(value);
+    }
+
+    /**
+     * Adds all elements from another list to this list, incrementing the totalAdded count.
+     *
+     * @param otherList the list from which elements are to be added
+     */
+    @Override
+    public boolean addAll(IntegerList otherList) {
+        return super.addAll(otherList);
+    }
+
+    /**
+     * Gets the total number of attempted insertions.
+     *
+     * @return the total number of attempted insertions
+     */
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
 }
